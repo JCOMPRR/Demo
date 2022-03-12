2d1509c5c5b468953618905133d3e03e7f022b33
Jose Rodriguez <tai-jo2010@hotmail.com>
2022-03-12 14:08:33 -0700

    Declaracion de un arreglo

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] nombrePersonas = { "Juan", "Pedro" };
+
+Console.WriteLine(nombrePersonas[1]);
+
